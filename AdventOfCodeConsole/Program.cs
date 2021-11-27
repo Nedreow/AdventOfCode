b102df6c187149707565f9e28868cabce813dc78
Nedreow <F.vanwoerden23@gmail.com>
2021-11-27 17:04:14 +0100

    Create project structure

---
+++ AdventOfCodeConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCodeConsole.Missions;
+
+namespace AdventOfCodeConsole // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? input = null;
+            while (string.IsNullOrEmpty(input))
+            {
+                Console.Write("Please paste input: ");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
